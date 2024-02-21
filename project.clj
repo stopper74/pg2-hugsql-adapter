b1a2eb98bf02b756a618d71421e19b55c828ed77
stopper <live96ru@yandex.ru>
2024-02-22 01:00:37 +0500

    init

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject stopper74.pg2-hugsql-adapter "0.0.1"
+  :dependencies [[org.clojure/clojure "1.12.0-alpha4"]
+                 [com.layerware/hugsql "0.5.1"]
+                 [com.github.igrishaev/pg2-core "0.1.3"]])
